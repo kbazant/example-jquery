f162184232d4dfcf87e9b9dcb9a451dbfdee8df4
kbazant <krizz100@yahoo.com>
2016-12-04 22:54:23 +0000

    added reset the form fields

--- app.js
+++ app.js
@@ -9,25 +9,30 @@ $('.loc').hover(
 //Adds a pet to the page with user input on button click
 $('#add-pet').on('click', function () {
 	// Grab info from the form
-	var $name = $('#pet-name').val();
-	var $species = $('#pet-species').val();
-	var $notes = $('#pet-notes').val();
+	var $name = $('#pet-name');
+	var $species = $('#pet-species');
+	var $notes = $('#pet-notes');
 
 	//Assemble the HTML of our new element with the above variables
 	var $newPet = $(
-		'<section class="six columns"><div class="card"><p><strong>Name:</strong> ' + $name +
-		'</p><p><strong>Species:</strong> ' + $species +
-		'</p><p><strong>Notes:</strong> ' + $notes +
+		'<section class="six columns"><div class="card"><p><strong>Name:</strong> ' + $name.val() +
+		'</p><p><strong>Species:</strong> ' + $species.val() +
+		'</p><p><strong>Notes:</strong> ' + $notes.val() +
 		'</p><span class="close">&times;</span></div></section>'
 	);
 
 	//Attach the new element to the page
 	$('#posted-pets').append($newPet);
+	
+	// Reset form fields
+  $name.val("");
+  $species.val("Dog");
+  $notes.val("");
 });
 
 // Images fade in
 $('img').css('display', 'none').fadeIn(1600);
 
-$('.card').on('click', function(){
+$('.card').on('click', function () {
 	$(this).toggleClass('selected');
 });
